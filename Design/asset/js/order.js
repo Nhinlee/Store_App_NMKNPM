0304f310643ef982fc27900cd9b11eaf14cb13e2
nhin lê chí <nhinlechi@gmail.com>
2019-12-13 19:14:14 +0700

    Login, Usecase-Choice, Order layouts demo

---
+++ Design/asset/js/order.js
@@ -0,0 +1,28 @@
+$(".back-button").click(function() {
+  $("body").load("usecase-choice.html");
+});
+
+$(".add-button").click(function() {
+  $("#bill-out").before(`<div class="item clearfix" id="expense-0">
+                          <div class="product-title">Apartment rent</div>
+                          <div class="right clearfix">
+                              <div class="item__value">- 900.00</div>
+                              <div class="item__number bg-success text-white">1</div>
+                              <div class="item__delete"  onclick = "rmOrder()">
+                                  <button class="item__delete--btn"><i class="far fa-times-circle mt-0"></i></button>
+                              </div>
+                          </div>
+                        </div>`);
+});
+
+function rmOrder() {
+  // FIX BUG DELETE ALL BILL
+  if (
+    $(event.target)
+      .html()
+      .toString() !== ""
+  )
+    return;
+  const orderItem = event.target.parentNode.parentNode.parentNode.parentNode;
+  $(orderItem).remove();
+}
